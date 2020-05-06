55322de8e6438ca19aeff7e3d7ecf3cff5430941
AirosS <61162349+AirosS@users.noreply.github.com>
2020-05-06 03:36:46 +0300

    task

--- src/main/kotlin/subtask1/PolynomialConverter.kt
+++ src/main/kotlin/subtask1/PolynomialConverter.kt
@@ -1,9 +1,43 @@
 package subtask1
-
+import kotlin.math.abs
 class PolynomialConverter {
 
-    // TODO: Complete the following function
     fun convertToStringFrom(numbers: Array<Int>): String? {
-        throw NotImplementedError("Not implemented")
+        if (numbers.isNullOrEmpty()) return null
+        var result =""
+        var i=0
+        var k =numbers.size-1
+        for(i in numbers.indices) {
+            if (numbers[i] != 0) {
+                if (numbers[i] < 0) {
+                    result = result + " - "
+                }
+                if ((numbers[i] > 0) && (i != 0)) {
+                    result = result + " + "
+                }
+                if ((numbers[i] != 1) && (numbers[i] != -1)) {
+                    result = result + abs(numbers[i])
+                }
+                if(k>1){
+                if (i == numbers.size) {
+                    result = result + "x"
+                }
+                if (i != numbers.size) {
+                    result = result + "x^" + k
+                }}
+                if(k==1)
+                {if (i == numbers.size) {
+                    result = result + "x"
+                }
+                    if (i != numbers.size) {
+                        result = result + "x"
+                    }}
+                k--
+            }
+            if(numbers[i]==0)
+            {
+            k--}
+        }
+        return result.toString()
     }
 }
