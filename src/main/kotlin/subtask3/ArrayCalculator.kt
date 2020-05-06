2b2d7d97a927aa2c8b9f5e2aef9f203c37ba99b9
AirosS <61162349+AirosS@users.noreply.github.com>
2020-05-06 22:38:14 +0300

    task

--- src/main/kotlin/subtask3/ArrayCalculator.kt
+++ src/main/kotlin/subtask3/ArrayCalculator.kt
@@ -1,9 +1,19 @@
 package subtask3
-
+import java.lang.Integer.max
+import kotlin.math.absoluteValue
 class ArrayCalculator {
-
-    // TODO: Complete the following function
     fun maxProductOf(numberOfItems: Int, itemsFromArray: Array<Any>): Int {
-        throw NotImplementedError("Not implemented")
+        var inresult: Array<Int> = itemsFromArray.filterIsInstance<Int>().toTypedArray()
+        val ind = numberOfItems
+        var outresult=1
+        if(inresult.size>0)
+        {
+            if(ind>inresult.size)
+            {
+                for(i in inresult)  outresult *=i
+                return outresult
+            }
+        }
+        return 0
     }
 }
